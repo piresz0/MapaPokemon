e1d6fafccc37181009e6d487daa6a56e6806a31b
renan <renan090604@edu.unipar.br>
2025-04-21 17:45:00 -0300

    21-04-2025

---
+++ src/Main.kt
@@ -0,0 +1,673 @@
+fun main(){
+    val bulbasaur = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val ivysaur = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val venusaur = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val charmander = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal")
+    )
+
+    val charmeleon = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal")
+    )
+
+    val charizard = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal"),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val squirtle = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val wartortle = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val blastoise = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val caterpie = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno")
+    )
+
+    val metapod = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno")
+    )
+
+    val butterfree = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno"),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val weedle = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val kakuna = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val beedrill = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val pidgey = mapOf(
+        "NORMAL" to listOf(),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val pidgeotto = mapOf(
+        "NORMAL" to listOf(),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val pidgeot = mapOf(
+        "NORMAL" to listOf(),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val rattata = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val raticate = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val spearow = mapOf(
+        "NORMAL" to listOf(),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val fearow = mapOf(
+        "NORMAL" to listOf(),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val ekans = mapOf(
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val arbok = mapOf(
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val pikachu = mapOf(
+        "ELETRICO" to listOf("Agua", "Voador")
+    )
+
+    val raichu = mapOf(
+        "ELETRICO" to listOf("Agua", "Voador")
+    )
+
+    val sandshrew = mapOf(
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val sandslash = mapOf(
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val nidoranf = mapOf(
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val nidorina = mapOf(
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val nidoqueen = mapOf(
+        "VENENO" to listOf("Grama", "Fada"),
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val nidoranm = mapOf(
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val nidorino = mapOf(
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val nidoking = mapOf(
+        "VENENO" to listOf("Grama", "Fada"),
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val clefairy = mapOf(
+        "FADA" to listOf("Lutador", "Dragao", "Noturno")
+    )
+
+    val clefable = mapOf(
+        "FADA" to listOf("Lutador", "Dragao", "Noturno")
+    )
+
+    val vulpix = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal")
+    )
+
+    val ninetales = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal")
+    )
+
+    val jigglypuff = mapOf(
+        "NORMAL" to listOf(),
+        "FADA" to listOf("Lutador", "Dragao", "Noturno")
+    )
+
+    val wigglytuff = mapOf(
+        "NORMAL" to listOf(),
+        "FADA" to listOf("Lutador", "Dragao", "Noturno")
+    )
+
+    val zubat = mapOf(
+        "VENENO" to listOf("Grama", "Fada"),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val golbat = mapOf(
+        "VENENO" to listOf("Grama", "Fada"),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val oddish = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val gloom = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val vileplume = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val paras = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno"),
+        "GRAMA" to listOf("Agua", "Terra", "Rocha")
+    )
+
+    val parasect = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno"),
+        "GRAMA" to listOf("Agua", "Terra", "Rocha")
+    )
+
+    val venonat = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val venomoth = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val diglett = mapOf(
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val dugtrio = mapOf(
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val meowth = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val persian = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val psyduck = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val golduck = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val mankey = mapOf(
+        "LUTADOR" to listOf("Normal", "Gelo", "Pedra", "Noturno", "Metal")
+    )
+
+    val primeape = mapOf(
+        "LUTADOR" to listOf("Normal", "Gelo", "Pedra", "Noturno", "Metal")
+    )
+
+    val growlithe = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal")
+    )
+
+    val arcanine = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal")
+    )
+
+    val poliwag = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val poliwhirl = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val poliwrath = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha"),
+        "LUTADOR" to listOf("Normal", "Gelo", "Pedra", "Noturno", "Metal")
+    )
+
+    val abra = mapOf(
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val kadabra = mapOf(
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val alakazam = mapOf(
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val machop = mapOf(
+        "LUTADOR" to listOf("Normal", "Gelo", "Pedra", "Noturno", "Metal")
+    )
+
+    val machoke = mapOf(
+        "LUTADOR" to listOf("Normal", "Gelo", "Pedra", "Noturno", "Metal")
+    )
+
+    val machamp = mapOf(
+        "LUTADOR" to listOf("Normal", "Gelo", "Pedra", "Noturno", "Metal")
+    )
+
+    val bellsprout = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val weepinbell = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val victreebel = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val tentacool = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val tentacruel = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val geodude = mapOf(
+        "PEDRA" to listOf(),
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val graveler = mapOf(
+        "PEDRA" to listOf(),
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val golem = mapOf(
+        "PEDRA" to listOf(),
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val ponyta = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal")
+    )
+
+    val rapidash = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal")
+    )
+
+    val slowpoke = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha"),
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val slowbro = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha"),
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val magnemite = mapOf(
+        "ELETRICO" to listOf("Agua", "Voador"),
+        "METAL" to listOf("Gelo", "Rocha", "Fada")
+    )
+
+    val magneton = mapOf(
+        "ELETRICO" to listOf("Agua", "Voador"),
+        "METAL" to listOf("Gelo", "Rocha", "Fada")
+    )
+
+    val farfetchd = mapOf(
+        "NORMAL" to listOf(),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val doduo = mapOf(
+        "NORMAL" to listOf(),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val dodrio = mapOf(
+        "NORMAL" to listOf(),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val seel = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val dewgong = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha"),
+        "GELO" to listOf("Grama", "Terra", "Voador", "Dragao")
+    )
+
+    val grimer = mapOf(
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val muk = mapOf(
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val shellder = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val cloyster = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha"),
+        "GELO" to listOf("Grama", "Terra", "Voador", "Dragao")
+    )
+
+    val gastly = mapOf(
+        "FANTASMA" to listOf("Psiquico", "Fantasma"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val haunter = mapOf(
+        "FANTASMA" to listOf("Psiquico", "Fantasma"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val gengar = mapOf(
+        "FANTASMA" to listOf("Psiquico", "Fantasma"),
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val onix = mapOf(
+        "PEDRA" to listOf(),
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val drowzee = mapOf(
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val hypno = mapOf(
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val krabby = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val kingler = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val voltorb = mapOf(
+        "ELETRICO" to listOf("Agua", "Voador")
+    )
+
+    val electrode = mapOf(
+        "ELETRICO" to listOf("Agua", "Voador")
+    )
+
+    val exeggcute = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val exeggutor = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha"),
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val cubone = mapOf(
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val marowak = mapOf(
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal")
+    )
+
+    val hitmonlee = mapOf(
+        "LUTADOR" to listOf("Normal", "Gelo", "Pedra", "Noturno", "Metal")
+    )
+
+    val hitmonchan = mapOf(
+        "LUTADOR" to listOf("Normal", "Gelo", "Pedra", "Noturno", "Metal")
+    )
+
+    val lickitung = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val koffing = mapOf(
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val weezing = mapOf(
+        "VENENO" to listOf("Grama", "Fada")
+    )
+
+    val rhyhorn = mapOf(
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal"),
+        "PEDRA" to listOf()
+    )
+
+    val rhydon = mapOf(
+        "TERRA" to listOf("Fogo", "Eletrico", "Veneno", "Rocha", "Metal"),
+        "PEDRA" to listOf()
+    )
+
+    val chansey = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val tangela = mapOf(
+        "GRAMA" to listOf("Agua", "Terra", "Rocha")
+    )
+
+    val kangaskhan = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val horsea = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val seadra = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val goldeen = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val seaking = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val staryu = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val starmie = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha"),
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val mrmime = mapOf(
+        "PSIQUICO" to listOf("Lutador", "Veneno"),
+        "FADA" to listOf("Lutador", "Dragao", "Noturno")
+    )
+
+    val scyther = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno"),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val jynx = mapOf(
+        "GELO" to listOf("Grama", "Terra", "Voador", "Dragao"),
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val electabuzz = mapOf(
+        "ELETRICO" to listOf("Agua", "Voador")
+    )
+
+    val magmar = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal")
+    )
+
+    val pinsir = mapOf(
+        "INSETO" to listOf("Grama", "Psiquico", "Noturno")
+    )
+
+    val tauros = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val magikarp = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val gyarados = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha"),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val lapras = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha"),
+        "GELO" to listOf("Grama", "Terra", "Voador", "Dragao")
+    )
+
+    val ditto = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val eevee = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val vaporeon = mapOf(
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val jolteon = mapOf(
+        "ELETRICO" to listOf("Agua", "Voador")
+    )
+
+    val flareon = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal")
+    )
+
+    val porygon = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val omanyte = mapOf(
+        "PEDRA" to listOf(),
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val omastar = mapOf(
+        "PEDRA" to listOf(),
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val kabuto = mapOf(
+        "PEDRA" to listOf(),
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val kabutops = mapOf(
+        "PEDRA" to listOf(),
+        "AGUA" to listOf("Fogo", "Terra", "Rocha")
+    )
+
+    val aerodactyl = mapOf(
+        "PEDRA" to listOf(),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val snorlax = mapOf(
+        "NORMAL" to listOf("")
+    )
+
+    val articuno = mapOf(
+        "GELO" to listOf("Grama", "Terra", "Voador", "Dragao"),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val zapdos = mapOf(
+        "ELETRICO" to listOf("Agua", "Voador"),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val moltres = mapOf(
+        "FOGO" to listOf("Planta", "Gelo", "Inseto", "Metal"),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val dratini = mapOf(
+        "DRAGAO" to listOf("Dragao")
+    )
+
+    val dragonair = mapOf(
+        "DRAGAO" to listOf("Dragao")
+    )
+
+    val dragonite = mapOf(
+        "DRAGAO" to listOf("Dragao"),
+        "VOADOR" to listOf("Grama", "Lutador", "Inseto")
+    )
+
+    val mewtwo = mapOf(
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+    val mew = mapOf(
+        "PSIQUICO" to listOf("Lutador", "Veneno")
+    )
+
+}
